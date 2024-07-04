245041188599c7ddc13b3e9b1662444f963d6227
jm-316 <yoyo3302@gmail.com>
2024-07-04 11:01:08 +0900

    marker 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import Map from "@/components/Map";
+import Markers from "@/components/Markers";
+import { useState } from "react";
+import * as stores from "@/data/store_data.json";
 
 export default function Home() {
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores["item"];
   return (
     <>
-      <Map />
+      <Map setMap={setMap} />
+      <Markers
+        map={map}
+        storeDatas={storeDatas}
+        setCurrentStore={setCurrentStore}
+      />
     </>
   );
 }
