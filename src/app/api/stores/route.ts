0c238d3c3414cc0707c250b1952bb7b721b82e6f
jm-316 <yoyo3302@gmail.com>
2024-07-06 16:53:14 +0900

    GET API page 관련 추가

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -1,12 +1,38 @@
-import prisma from "@/db";
 import { NextResponse } from "next/server";
+import prisma from "@/db";
 
 export async function GET(req: Request) {
-  const stores = await prisma.store.findMany({
-    orderBy: { id: "asc" },
-  });
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
+  const limit = searchParams.get("limit") as string;
 
-  return NextResponse.json(stores, {
-    status: 200,
-  });
+  if (page) {
+    const count = await prisma.store.count();
+    const skipPage = parseInt(page) - 1;
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+      take: parseInt(limit),
+      skip: skipPage * 10,
+    });
+
+    return NextResponse.json(
+      {
+        page: parseInt(page),
+        data: stores,
+        totalCount: count,
+        totalPage: Math.ceil(count / 10),
+      },
+      {
+        status: 200,
+      }
+    );
+  } else {
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    return NextResponse.json(stores, {
+      status: 200,
+    });
+  }
 }
