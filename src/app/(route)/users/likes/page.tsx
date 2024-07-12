c5633bd4228416ec226697c5d7faaf58dd965404
jm-316 <yoyo3302@gmail.com>
2024-07-12 15:20:39 +0900

    LikesPage 작성

--- src/app/(route)/users/likes/page.tsx
+++ src/app/(route)/users/likes/page.tsx
@@ -1,3 +1,65 @@
-export default function LikesPage() {
-  return <div>LikesPage</div>;
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { LikeApiResponse, LikeInterface } from "@/interface";
+import StoreList from "@/components/StoreList";
+import Loading from "@/components/Loading";
+import Pagination from "@/components/Pagination";
+
+export default function LikesPage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = searchParams?.page || "1";
+
+  const fetchLikes = async () => {
+    const { data } = await axios(`/api/likes?limit=10&page=${page}`);
+    return data as LikeApiResponse;
+  };
+
+  const {
+    data: likes,
+    isError,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: [`likes-${page}`],
+    queryFn: fetchLikes,
+  });
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요.
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-4 md:max-w-4xl mx-auto py-8">
+      <h3 className="text-lg font-semibold">찜한 맛집</h3>
+      <div className="mt-1 text-gray-500 text-sm">찜한 가게 리스트입니다.</div>
+      <ul role="list" className="divide-y divide-gray-100 mt-10">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          likes?.data.map((like: LikeInterface, index) => (
+            <StoreList store={like.store} key={index} />
+          ))
+        )}
+        {isSuccess && !!!likes.data.length && (
+          <div className="p-4 border border-gray-200 rounded-md text-sm text-gray-400">
+            찜한 가게가 없습니다.
+          </div>
+        )}
+      </ul>
+      <Pagination
+        total={likes?.totalPage}
+        page={page}
+        pathname="/users/likes"
+      />
+    </div>
+  );
 }
