397d155db2039ee08f93f8a25b4157fd11423afe
jm-316 <yoyo3302@gmail.com>
2024-07-13 11:46:12 +0900

    myPage 구현

--- src/app/(route)/users/mypage/page.tsx
+++ src/app/(route)/users/mypage/page.tsx
@@ -1,5 +1,108 @@
-import React from "react";
+"use client";
 
-export default function MyPage() {
-  return <div>MyPage</div>;
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { signOut, useSession } from "next-auth/react";
+import { StoreApiResponse } from "@/interface";
+import Pagination from "@/components/Pagination";
+import StoreList from "@/components/StoreList";
+
+export default function MyPage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const { data: session } = useSession();
+  const page = searchParams?.page || "1";
+
+  const fetchStores = async () => {
+    const { data } = await axios(
+      `/api/stores?&limit=10&page=${page}&mypage=${session?.user.id}`
+    );
+
+    return data as StoreApiResponse;
+  };
+
+  const { data: stores } = useQuery({
+    queryKey: [`store-${page}`],
+    queryFn: fetchStores,
+  });
+
+  return (
+    <div className="md:max-w-5xl mx-auto px-4 py-8">
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          마이페이지
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          사용자 기본정보
+        </p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              이름
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {session?.user.name ?? "사용자"}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              이메일
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {session?.user.email}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              이미지
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <img
+                alt="프로필 이미지"
+                width={48}
+                height={48}
+                className="rounded-full h-12 w-12"
+                src={session?.user.image || "/images/markers/default.png"}
+              />
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              설정
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <button
+                type="button"
+                className="underline hover:text-gray-500"
+                onClick={() => signOut()}>
+                로그아웃
+              </button>
+            </dd>
+          </div>
+        </dl>
+      </div>
+      <div className="mt-8 px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          내가 등록한 가게
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          가게 리스트
+        </p>
+      </div>
+      <div className="divide-y divide-gray-300 my-10">
+        {stores?.data?.map((store) => (
+          <StoreList store={store} key={store.id} />
+        ))}
+      </div>
+      <Pagination
+        total={stores?.totalPage}
+        page={page}
+        pathname="/users/mypage"
+      />
+    </div>
+  );
 }
